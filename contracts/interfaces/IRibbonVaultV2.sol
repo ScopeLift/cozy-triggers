30b9affcdc0e85d39aca09a039d0382ce10b535f
Lawrence Wu <lawrence@lawrencewu.me>
2021-09-15 14:41:53 -0400

    Add ribbon trigger
    Adds the trigger for the Ribbon theta vault ETH vault + tests
    
    Fix typo

---
+++ contracts/interfaces/IRibbonVaultV2.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.5;
+
+// Ribbon vault
+interface IRibbonVaultV2 {
+  function totalSupply() external view returns (uint256);
+
+  function pricePerShare() external view returns (uint256);
+}
