d7111295dfd3a6e917a8fa99f4252ddecb2a980f
chad-js <chadflemies@gmail.com>
2022-01-10 15:45:39 -0500

    convex frax & alUSD pr nits

--- scripts/storage-slots.sh
+++ scripts/storage-slots.sh
@@ -4,23 +4,25 @@ token=0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B # fraxCrv
 holder=0x99780beadd209cc3c7282536883ef58f4ff4e52f
 
 # balanceOf
-# for slot in {0..250}; do
-#   echo "slot:" $slot
-#   balanceOf=$(seth call $token "balanceOf(address)(uint256)" $holder)
-#   computedSlot=$(seth index uint256 address $slot $holder) # for Vyper tokens
-#   # computedSlot=$(seth index address uint256 $holder $slot)
-#   storageSlotVal=$(seth --to-dec $(seth storage $token $computedSlot))
-#   [[ $balanceOf -eq $storageSlotVal ]] && { echo "balanceOf:" $balanceOf; echo "storageSlotVal:" $storageSlotVal; }
-#   [[ $balanceOf -eq $storageSlotVal ]] && { echo "MAPPING SLOT:" $slot; exit 1; }
-# done
+echo "balanceOf..."
+for slot in {0..250}; do
+  balanceOf=$(seth call $token "balanceOf(address)(uint256)" $holder)
+  computedSlot=$(seth index uint256 address $slot $holder) # for Vyper tokens
+  # computedSlot=$(seth index address uint256 $holder $slot)
+  storageSlotVal=$(seth --to-dec $(seth storage $token $computedSlot))
+  [[ $balanceOf -eq $storageSlotVal ]] && { echo "token balanceOf():" $balanceOf; echo "balanceOf storageSlotVal:" $storageSlotVal; }
+  [[ $balanceOf -eq $storageSlotVal ]] && { echo "balanceOf MAPPING SLOT:" $slot; break; }
+done
 
+echo "================================"
 
 # totalSupply
-# for slot in {0..250}; do
-#   echo "slot:" $slot
-#   totalSupply=$(seth call $token "totalSupply()(uint256)" $holder)
-#   storageSlotVal=$(seth --to-dec $(seth storage $token $slot))
-#   [[ $totalSupply -eq $storageSlotVal ]] && { echo "totalSupply:" $totalSupply; echo "storageSlotVal:" $storageSlotVal; }
-#   [[ $totalSupply -eq $storageSlotVal ]] && { echo "MAPPING SLOT:" $slot; exit 1; }
-# done
+echo "totalSupply..."
+for slot in {0..250}; do
+  totalSupply=$(seth call $token "totalSupply()(uint256)")
+  storageSlotVal=$(seth --to-dec $(seth storage $token $slot))
+  [[ $totalSupply -eq $storageSlotVal ]] && { echo "token totalSupply():" $totalSupply; echo "totalSupply storageSlotVal:" $storageSlotVal; }
+  [[ $totalSupply -eq $storageSlotVal ]] && { echo "totalSupply MAPPING SLOT:" $slot; break; }
+done
 
+exit 1;
